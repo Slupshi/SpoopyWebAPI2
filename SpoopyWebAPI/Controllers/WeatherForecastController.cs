85ef26396b5fb514492580855440d68e175ed2b5
Slupshi <slupshi@gmail.com>
2023-03-06 19:05:04 +0100

    Add project files.

---
+++ SpoopyWebAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,53 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpoopyWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    [Produces("application/json")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        /// <summary>
+        /// Get a weather forecast item
+        /// </summary>
+        /// <returns></returns>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Todo
+        ///     {
+        ///        "id": 1,
+        ///        "name": "Item #1",
+        ///        "isComplete": true
+        ///     }
+        ///
+        /// </remarks>
+        /// <response code="201">Returns the newly created item</response>
+        /// <response code="400">If the item is null</response>
+        [HttpGet(Name = "GetWeatherForecast")]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ SpoopyWebAPI/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.OpenApi.Models;
+
+namespace SpoopyWebAPI;
+
+public class Startup
+{
+    private readonly IConfiguration _configuration;
+
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddMvc();
+        services.AddSession();
+        services.AddHttpLogging((options) => {
+            options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.Request;
+        });
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v2", new OpenApiInfo
+            {
+                Version = "v2",
+                Title = "SpoopyWebAPI",
+                Description = "An ASP.NET Core Web API for managing Spoopy DiscordBot",
+            });
+
+            var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+            options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+        });
+    }
+
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options => {
+            options.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
+            options.RoutePrefix = string.Empty;            
+        });
+
+        app.UseHttpLogging();
+
+        app.UseHttpsRedirection();
+
+        app.UseSession();
+
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints => {
+            endpoints.MapControllers();
+        });
+    }
+
+    
+}
+
